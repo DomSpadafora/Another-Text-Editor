86ff920e5bed8a2c544eb09f6476447c0169e722
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-03-01 22:04:17 -0500

    initial push to heroku

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,4 +1,4 @@
-const { warmStrategyCache } = require('workbox-recipes');
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
@@ -26,7 +26,7 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
